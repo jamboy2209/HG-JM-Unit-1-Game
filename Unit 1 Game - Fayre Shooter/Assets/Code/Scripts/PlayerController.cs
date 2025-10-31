3422bb2fff22b3c6f3d32d8ae80e2d13000f528f
HAMISH GLENCROSS <2501222@uad.ac.uk>
2025-10-31 09:35:12 +0000

    Added Raycasting Hit Object Detection and modified Docs

--- Unit 1 Game - Fayre Shooter/Assets/Code/Scripts/PlayerController.cs
+++ Unit 1 Game - Fayre Shooter/Assets/Code/Scripts/PlayerController.cs
@@ -3,18 +3,41 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //=======================
+    //Variable Initialisation
+    //=======================
+
+    //Movement
+
+    public float MoveSpeed = 10.0f;
+    private Vector2 movementInput;
+    
+    //Camera Movement
+    
     public Camera camera;
 
     public Transform head;
     public float Senstivity = 10.0f;
-    public float MoveSpeed = 10.0f;
-    private Vector2 movementInput;
+    
     private Vector2 lookInput;
-
+    
     private float pitch = 0.0f;
     private float yaw = 0.0f;
 
+    //Raycasting
+
+    public float m_RayDistance = 10.0f;
+
+    private bool m_RayHit = false;
+    private Vector3 m_HitPoint = Vector3.zero;
+    private Vector3 m_HitNormal = Vector3.zero;
+    private bool m_Grounded = false;
+
     private CharacterController characterController;
+
+    //============
+    //Main Program
+    //============
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -45,7 +68,29 @@ public class PlayerController : MonoBehaviour
         movement.y = 0.0f; //Stops us from flying
         movement.Normalize(); //Turns this into a Unit Vector and stops us from moving faster diagonally
 
-        characterController.Move(movement * Time.deltaTime * MoveSpeed);
+        characterController.Move(movement * Time.deltaTime * MoveSpeed);       
+    }
+
+    void DoRaycast()
+    {
+        RaycastHit hitInfo; //gives us information about what we hit (if anything)
+        Ray ray = new Ray(transform.position, head.forward);
+
+        //Do the raycast. Store the information in hitInfo
+        m_RayHit = Physics.Raycast(ray, out hitInfo, m_RayDistance);
+
+        if (m_RayHit)
+        {
+            //m_HitPoint = hitInfo.point;     //Store the position that our ray collided with the object
+            //m_HitNormal = hitInfo.normal;   //Store the surface normal of the object
+            //m_Grounded = Vector3.Dot(Vector3.up, hitInfo.normal) > 0.5f; //Bit of a magic number here. Just trust me on this one.
+
+            Debug.Log("The ray hit" + hitInfo.collider.name);
+        }
+        else
+        {
+            Debug.Log("The ray hit nothing!");
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -67,6 +112,9 @@ public class PlayerController : MonoBehaviour
     public void OnAttack(InputAction.CallbackContext context)
     {
         Debug.Log("Fire");
+
+        DoRaycast();
+
     }
 
     public void OnScope(InputAction.CallbackContext context)
